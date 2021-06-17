7161576caa0fcd33b960ea471745982cd6683051
Juliana <julianachristello@gmail.com>
2021-06-17 01:26:44 -0300

    CRUD Games

--- src/server.js
+++ src/server.js
@@ -49,6 +49,58 @@ app.post("/categories", async (req, res) => {
   }
 });
 
+app.get("/games", async (req, res) => {
+  const parameter = req.query.name;
+  try {
+    const games = await connection.query("SELECT * FROM games");
+    const data = games.rows;
+    if (parameter) {
+      const num = parameter.length;
+      const resultado = data.filter((e) => {
+        const split= e.name.slice(0, num);
+        return split === parameter;
+      });
+      console.log(resultado);
+      res.send(resultado);
+    }
+    res.send(games.rows);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/games", async (req, res) => {
+  const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+  try {
+    const games = await connection.query("SELECT * FROM games");
+    const data = games.rows;
+    const findGame = data.find((e) => e.name === name);
+
+    if (
+      name === "" ||
+      parseInt(stockTotal) === 0 ||
+      parseInt(pricePerDay) === 0
+    ) {
+      return res.sendStatus(400);
+    } else if (findGame) {
+      return res.sendStatus(409);
+    }
+    console.log(stockTotal);
+    console.log(findGame);
+    console.log(req.body);
+    console.log(data.includes(name));
+    await connection.query(
+      'INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)',
+      [name, image, stockTotal, categoryId, pricePerDay]
+    );
+    res.sendStatus(201);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+});
+
 app.listen(4000, () => {
   console.log("Server listening on port 4000. ");
 });
